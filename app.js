d4b31aaf5e84d1c0a409ad27b677727140a1d7a9
YUAN-TSUNG <satoshi.liu@icloud.com>
2024-03-04 23:46:16 +0900

    ✨ request handling

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
@@ -14,12 +15,27 @@ const server = http.createServer((req, res) => {
     res.write("</html>");
     return res.end();
   }
-  console.log(req);
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write(`<body><h1>hello this is ${pageName} page</h1></body>`);
-  res.write("</html>");
-  res.end();
+  if (url === "/message" && req.method === "POST") {
+    const body = [];
+    
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync('message.txt', message);
+    });
+    res.writeHead(302, { Location: "/" });
+    return res.end();
+  }
+  // console.log(req);
+  // res.write("<html>");
+  // res.write("<head><title>My First Page</title></head>");
+  // res.write(`<body><h1>hello this is ${pageName} page</h1></body>`);
+  // res.write("</html>");
+  // res.end();
 });
 
 server.listen(3000);
