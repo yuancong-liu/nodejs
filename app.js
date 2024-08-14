599631fbbacac9851b941f905c3e7759c0b76d05
YUAN-TSUNG <satoshi.liu@icloud.com>
2024-08-14 22:42:58 +0900

    😴 lets call it a day

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const rootDir = require("./utils/path");
@@ -12,7 +12,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, "public")));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -4,8 +4,10 @@ const express = require('express');
 const rootDir = require("../utils/path");
 
 const router = express.Router();
+const adminData = require("./admin");
 
 router.get('/', (req, res, next) => {
+  console.log(adminData.products);
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
