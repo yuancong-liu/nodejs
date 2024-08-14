2043ad79a9e0a6178698d0b4aafba3b53e6ae491
YUANCONG LIU <yuancong.liu@bit-realty.com>
2024-08-14 15:33:05 +0900

    ✨ npm initialized

--- app.js
+++ app.js
@@ -1,41 +1,6 @@
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const pageName = url.split("/")[1];
-
-  res.setHeader("Content-Type", "text/html");
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write(
-      `<body><form action="/message" method="POST"><input type="text" name="message"><button>Send</button></form></body>`
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && req.method === "POST") {
-    const body = [];
-    
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFileSync('message.txt', message);
-    });
-    res.writeHead(302, { Location: "/" });
-    return res.end();
-  }
-  // console.log(req);
-  // res.write("<html>");
-  // res.write("<head><title>My First Page</title></head>");
-  // res.write(`<body><h1>hello this is ${pageName} page</h1></body>`);
-  // res.write("</html>");
-  // res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
