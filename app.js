9ca008fae5de29c44f910889fba760ad71f7be07
YUANCONG LIU <yuancong.liu@bit-realty.com>
2024-08-14 20:47:00 +0900

    🚄 express added

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const http = require("http");
-const routes = require("./routes");
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const server = http.createServer(routes);
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-server.listen(3000);
+const rootDir = require("./utils/path");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, "public")));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+});
+
+app.listen(3000);
